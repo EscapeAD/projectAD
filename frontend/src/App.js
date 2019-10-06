2f42ce827cf062040139c008532ddf20c9c45214
Adam Tse <mr.adam.tse@gmail.com>
2019-10-07 00:37:24 +0800

    react add bulma + sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App section">
+      <figure className="image is-128x128">
+        <img className="image is-rounded" src={logo} alt="logo" />
+      </figure>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <button className="button is-danger is-outlined">
+        Hello
+      </button>
     </div>
   );
 }
