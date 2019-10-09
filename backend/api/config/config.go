1b34bd847f7985333dd8363cb99bfba36e43d633
Adam Tse <mr.adam.tse@gmail.com>
2019-10-10 01:43:40 +0800

    basic config setup

---
+++ backend/api/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+var (
+	PORT = 0
+)
+
+// Load - Load server config
+func Load() {
+	var err error
+	PORT, err = strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		log.Println(err)
+		// Default 9000
+		PORT = 9000
+	}
+}
--- backend/api/server.go
+++ backend/api/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"escapead/projectAD/backend/api/config"
 	"escapead/projectAD/backend/api/router"
 	"fmt"
 	"log"
@@ -9,8 +10,13 @@ import (
 
 // Run start server
 func Run() {
-	port := ":3000"
-	fmt.Println("Online and Listening on port", port)
+	config.Load()
+	fmt.Println("Online and Listening on port", config.PORT)
+	Listen(config.PORT)
+}
+
+// Listen - Load Server
+func Listen(port int) {
 	r := router.NEW()
-	log.Fatal(http.ListenAndServe(port, r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
 }
