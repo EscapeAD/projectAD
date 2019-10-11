8af68fae9bae02f318dc97f695c5efd2a6511179
Adam Tse <mr.adam.tse@gmail.com>
2019-10-12 02:23:33 +0800

    env settings

--- backend/api/config/config.go
+++ backend/api/config/config.go
@@ -4,17 +4,26 @@ import (
 	"log"
 	"os"
 	"strconv"
+
+	godotenv "github.com/joho/godotenv/autoload"
 )
 
 var (
 	//PORT - api port
 	PORT = 0
+	// DBURL - DB URL
+	DBURL = ""
 )
 
 // Load - Load server config
 func Load() {
 	var err error
-	PORT, err = strconv.Atoi(os.Getenv("PORT"))
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	PORT, err = strconv.Atoi(os.Getenv("API_PORT"))
 	if err != nil {
 		log.Println(err)
 		// Default 9000
--- backend/go.mod
+++ backend/go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
 	github.com/jinzhu/gorm v1.9.11 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 )
