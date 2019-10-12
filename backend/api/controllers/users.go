b0333d63fc68c8be2ebe33be705f2aaffe1ff127
Adam Tse <mr.adam.tse@gmail.com>
2019-10-12 20:56:05 +0800

    BE: route Add user

--- backend/api/controllers/users.go
+++ backend/api/controllers/users.go
@@ -1,7 +1,14 @@
 package controllers
 
 import (
+	"encoding/json"
+	"escapead/projectAD/backend/api/database"
+	"escapead/projectAD/backend/api/models"
+	"escapead/projectAD/backend/api/repository"
+	"escapead/projectAD/backend/api/repository/crud"
 	_response "escapead/projectAD/backend/api/responses"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -12,7 +19,33 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 
 // CreateUser - GET user
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	_response.JSON(w, http.StatusOK, map[string]string{})
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		_response.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		_response.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	db, err := database.Connect()
+	if err != nil {
+		_response.Error(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	repo := crud.NewRepositoryUsersCRUD(db)
+	func(userRepository repository.UserRepository) {
+		user, err = userRepository.Save(user)
+		if err != nil {
+			_response.Error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		w.Header().Set("Location", fmt.Sprintf("%s%s%d", r.Host, r.RequestURI, user.ID))
+		_response.JSON(w, http.StatusCreated, user)
+	}(repo)
 }
 
 // GetUser - GET an user
---
+++ backend/api/repository/user_repository.go
@@ -0,0 +1,12 @@
+package repository
+
+import "escapead/projectAD/backend/api/models"
+
+// UserRepository - interface
+type UserRepository interface {
+	Save(models.User) (models.User, error)
+	// FindAll() ([]models.User, error)
+	// FindById(uint32, models.User) (uint64, error)
+	// Update(uint32) (uint64, error)
+	// Delete(uint32) (uint64, error)
+}
