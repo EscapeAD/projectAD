79e3cc8c8dbc793948472c969c22f046d22f2c20
Atom <atom@Atoms-MacBook-Pro.local>
2024-08-11 01:54:47 +0800

    feat(AD): java 22

---
+++ backendv2/src/test/java/itsadamtse/com/idea/IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package itsadamtse.com.idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
